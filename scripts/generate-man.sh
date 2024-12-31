eb5d11f6af47304ca5262780aca27af144cd8e6c
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2024-12-31 16:03:30 +0000

    feat(man): add script to generate man page

---
+++ scripts/generate-man.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Copyright 2024 Ankur Sinha
+# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com> 
+# File : 
+#
+
+help2man -N ./calliope.sh -o ./man/man1/calliope.1 --version-option="-H"
